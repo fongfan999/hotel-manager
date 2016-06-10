aa9fd98cc68e471691482cdda0a8046a63bdedb2
Phong Phan <fongfan999@gmail.com>
2016-06-10 11:49:21 +0700

    Complete Admin Dashboard

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -14,6 +14,20 @@ class Bill < ActiveRecord::Base
 
   self.per_page = 10
 
+  def self.chart_by_week(cols)
+    hash = {}
+    results = Bill.order(created_at: :desc).group_by { |b| b.created_at.beginning_of_week }
+
+    results.each do |key, value|
+      tmp = {}
+      end_of_week = key.end_of_week
+      tmp["#{key.strftime('%d/%m')} - #{end_of_week.strftime('%d/%m')}"] = value.count
+      hash.merge!(tmp)
+    end
+
+    hash.to_a.reverse.first(cols).to_h
+  end
+
   def employee_name
     employee.admin? ? "Admin" : employee.employee.name
   end
