8cbaf1f755dca10e55cd0f8d413fc0507ffbc63c
Phong Phan <fongfan999@gmail.com>
2016-06-14 11:01:31 +0700

    Fix image url and title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 	def title(*parts)
 		unless parts.empty?
 			content_for :title do
-				(parts << "Hotel Manager").join(" - ")
+				(parts << "Palace Hotel").join(" - ")
 			end
 		end
 	end
