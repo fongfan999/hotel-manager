878e202f9d49d610b013c993099719bcaf391aa6
Phong Phan <fongfan999@gmail.com>
2016-06-14 11:13:11 +0700

    Fix title to Hostel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 	def title(*parts)
 		unless parts.empty?
 			content_for :title do
-				(parts << "Palace Hotel").join(" - ")
+				(parts << "Palace Hostel").join(" - ")
 			end
 		end
 	end
